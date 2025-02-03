2bb8d5ccad4e485e3da1f215bebac86c1408da09
AbramIV <septrakon@gmail.com>
2025-02-03 16:14:36 +0400

    transfer

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using ConsoleApp.Helpers;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
 
 List<EmailSender> emails = Enumerable.Range(0, 200).Select(n => new EmailSender()).ToList();
 var subject = "Logger test";
@@ -8,9 +12,29 @@ var recipients = "jorge.salazar@sks-textile.com;gerardo.nino@sks-textile.com";
 
 try
 {
-    
+    var tcpListener = new TcpListener(IPAddress.Parse("192.168.1.71"), 8888);
+    try
+    {
+        tcpListener.Start();    // запускаем сервер
+        Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+
+        while (true)
+        {
+            // получаем подключение в виде TcpClient
+            using var tcpClient = await tcpListener.AcceptTcpClientAsync();
+            Console.WriteLine($"Входящее подключение: {tcpClient.Client.RemoteEndPoint}");
+        }
+    }
+    finally
+    {
+        tcpListener.Stop(); // останавливаем сервер
+    }
 }
 catch (Exception ex)
 {
+    Console.WriteLine(ex.Message);
     File.WriteAllText(@"C:\Main\Test\logs.txt", ex.Message);
 }
+
+Console.WriteLine("Done!");
+Console.ReadLine();
